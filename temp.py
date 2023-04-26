d66669cf40f1d4660763e6c684a35f221a514ccc
rkp514 <64496499+rkp514@users.noreply.github.com>
2023-04-26 20:33:59 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,34 +1,41 @@
 import streamlit as st
 import altair as alt
-from vega_datasets import data
+import pandas as pd
 
-# Load data
-source = data.stocks()
+# Create example data
+data = pd.DataFrame({
+    'category': ['A', 'B', 'C', 'D', 'E'],
+    'value1': [10, 20, 30, 40, 50],
+    'value2': [20, 30, 40, 50, 60],
+    'value3': [30, 40, 50, 60, 70],
+})
+
+# Melt the data into long format
+melted_data = pd.melt(data, id_vars=['category'], var_name='variable', value_name='value')
 
 # Define a selection
-symbol_selection = alt.selection_multi(fields=['symbol'])
+category_selection = alt.selection_multi(fields=['category'])
 
 # Define the chart
-chart = alt.Chart(source).mark_line().encode(
-    x='date:T',
-    y='price:Q',
-    color=alt.condition(symbol_selection, 'symbol:N', alt.value('lightgray'), legend=None),
-    opacity=alt.condition(symbol_selection, alt.value(1), alt.value(0.1))
+chart = alt.Chart(melted_data).mark_bar().encode(
+    x='variable:N',
+    y='sum(value):Q',
+    color=alt.Color('category:N'),
+    opacity=alt.condition(category_selection, alt.value(1), alt.value(0.1))
 ).add_selection(
-    symbol_selection
+    category_selection
 ).properties(
     width=800,
     height=400
 )
 
 # Add the legend
-legend = alt.Chart(source).mark_point().encode(
-    y=alt.Y('symbol:N', axis=alt.Axis(orient='right')),
-    color=alt.Color('symbol:N', legend=None),
-    size=alt.value(50),
-    opacity=alt.condition(symbol_selection, alt.OpacityValue(1), alt.OpacityValue(0.1))
+legend = alt.Chart(melted_data).mark_rect().encode(
+    y=alt.Y('category:N', axis=alt.Axis(orient='right')),
+    color=alt.Color('category:N'),
+    opacity=alt.condition(category_selection, alt.OpacityValue(1), alt.OpacityValue(0.1))
 ).add_selection(
-    symbol_selection
+    category_selection
 )
 
 # Combine the chart and the legend

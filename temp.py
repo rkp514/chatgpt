3129395223a26041c78a419717ede94d1b7a1caf
rkp514 <64496499+rkp514@users.noreply.github.com>
2023-05-17 21:34:06 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,34 +1,36 @@
 import streamlit as st
-import altair as alt
 import pandas as pd
+import plotly.graph_objects as go
 
-# Create example data
+# Sample data
 data = pd.DataFrame({
-    'category': ['A', 'B', 'C', 'D', 'E'],
-    'value1': [10, 20, 30, 40, 50],
-    'value2': [20, 30, 40, 50, 60],
-    'value3': [30, 40, 50, 60, 70],
+    'Category': ['A', 'B', 'C'],
+    'Value': [10, 20, 30],
+    'Subcategory': ['A1', 'B1', 'C1'],
+    'Subvalue': [5, 15, 25]
 })
 
-# Melt the data into long format
-melted_data = pd.melt(data, id_vars=['category'], var_name='variable', value_name='value')
+# Render the initial pie chart
+def render_chart(df):
+    fig = go.Figure(data=[go.Pie(labels=df['Category'], values=df['Value'])])
+    st.plotly_chart(fig)
 
-# Define a selection
-category_selection = alt.selection_multi(fields=['category'])
+# Render the subcategory pie chart
+def render_subchart(df):
+    fig = go.Figure(data=[go.Pie(labels=df['Subcategory'], values=df['Subvalue'])])
+    st.plotly_chart(fig)
 
-# Define the chart
-chart = alt.Chart(melted_data).mark_bar().encode(
-    x='variable:N',
-    y='sum(value):Q',
-    color=alt.Color('category:N'),
-    opacity=alt.condition(category_selection, alt.value(1), alt.value(0.1)),
-    tooltip=[alt.Tooltip('variable:N', title='Variable'), alt.Tooltip('value:Q', title='Value')]
-).add_selection(
-    category_selection
-).properties(
-    width=800,
-    height=400,
-)
+# Main app
+def main():
+    st.title("Pie Chart Drilldown")
 
-# Display the chart in Streamlit
-st.altair_chart(chart, use_container_width=True)
+    # Render the initial pie chart
+    render_chart(data)
+
+    # Check if the user wants to drill down
+    if st.button("Drill down"):
+        # Render the subcategory pie chart
+        render_subchart(data)
+
+if __name__ == '__main__':
+    main()
